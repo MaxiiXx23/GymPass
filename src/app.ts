d8fc3ae7b581a6e448e35f9ea79b7058dfa767c9
MaxiiXx23 <max.232017@gmail.com>
2023-04-03 15:53:48 -0300

    struct: estruturação do Banco de dados através das Migrations e adição e configuração do docker-compose.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
 const app = express()
+const prisma = new PrismaClient()
 
 app.use(express.json())
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+
+// prisma.user.create({
+//   data: {
+//     email: 'max.123@hotmail.com',
+//     name: 'Max',
+//   },
+// })
 
 export { app }
